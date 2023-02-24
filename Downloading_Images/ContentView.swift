c35093da8e0d8b7af6ea4fac175595992f8f7ac5
Switch26 <sergueivinnitskii@gmail.com>
2023-02-24 10:23:00 -0500

    Initial Commit

---
+++ Downloading_Images/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Downloading_Images
+//
+//  Created by Serguei Vinnitskii on 2/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Downloading_Images/Downloading_ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Downloading_ImagesApp.swift
+//  Downloading_Images
+//
+//  Created by Serguei Vinnitskii on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Downloading_ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
